86a889a0dc2c67e6de464a83afa83ffabff932ac
Juan Diego Mejia Vargas <jdmejiav@eafit.edu.co>
2022-10-17 10:46:18 -0400

    delete fix

--- PlaneacionServer/index.js
+++ PlaneacionServer/index.js
@@ -73,7 +73,7 @@ async function main() {
         const fetchData = async (client, list) => {
             const result = await client.db("planeacion").collection("planeacion").findOne({ _id: list })
 
-            console.log(`El resultado seria ${result}`)
+            //console.log(`El resultado seria ${result}`)
             return result.data
         }
 
@@ -91,7 +91,7 @@ async function main() {
                 }
             )
 
-            console.log(`Resultado de update ${result}`)
+
 
         }
 
@@ -109,7 +109,7 @@ async function main() {
             console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
 
             connection.on('message', async function (message) {
-                console.log(data)
+                //console.log(data)
                 if (message.type === 'utf8') {
 
                     const input = JSON.parse(message.utf8Data);
@@ -120,64 +120,89 @@ async function main() {
                     } else if (input["type"] == 'add') {
                         data.push(input["data"])
                     } else if (input["type"] == 'delete') {
+
                         let row = input["row"]
+                        console.log(row)
                         data = input["data"]
-                        data.splice(data, row)
-                        for (let i = row; i < data.length; i++) {
-                            data[i].id = i + 1                        
-                        }
+                        const copy = [...data];
+                        const arr_inf = copy.splice(copy, row)
+                        console.log("______________________")
+                        console.log("arr_inf antes")
+                        console.log(arr_inf)
                         
-                    }
-                    for (key in clients) {
-
-
-
-
-                        if (input["type"] == 'update') {
-                            let row = input["row"]
-                            const retorno = {
-                                
-                                "type": "update",
-                                "data": data[row],
-                                "row": row
-                            }
-
-                            //console.log(data[row])
-                            clients[key].send(JSON.stringify(retorno));
-
-
-                        } else if (input["type"] == 'conn') {
-                            //console.log("En Conn")
-                            //console.log(data)
-                            const retorno = {
-                                "type": "conn",
-                                "data": data
-                            }
-                            clients[key].send(JSON.stringify(retorno));
-                        } else if (input["type"] == 'add') {
-                            
-                            const retorno = {
-                                "type": "add",
-                                "data": input["data"],
-                                "row": input["row"]
-                            }
-                            //await updateData(client, "sameday", data)
-                            clients[key].send(JSON.stringify(retorno));
-                        } else if (input["type"] == 'delete') {
-
-                            const retorno = {
-                                "type": "delete",
-                                "data": data
-                            }
-
-                            clients[key].send(JSON.stringify(retorno));
-                            //await updateData(client, "sameday", data)
-                            
-                            
+                        for (let i = row+1; i < data.length; i++) {
+                            arr_inf.push(data[i])
+                            arr_inf[arr_inf.length - 1].id = arr_inf[arr_inf.length - 1].id = i 
                         }
-                        //console.log("Al final")
+                        console.log("----------------------")
+                        console.log("arr_inf despues")
+                        console.log(arr_inf)
+                        console.log("______________________")
+                        
+
+                        data = arr_inf
+                        console.log("data")
+                        console.log(data)
+                    }
+
+                    for (key in clients) {
+                        if (clients[key].state === "open") {
+
+                            if (input["type"] == 'update') {
+                                let row = input["row"]
+                                const retorno = {
+
+                                    "type": "update",
+                                    "data": data[row],
+                                    "row": row
+                                }
+
+                                //console.log(data[row])
+                                clients[key].send(JSON.stringify(retorno));
 
 
+                            } else if (input["type"] == 'conn') {
+                                //console.log("En Conn")
+                                //console.log(data)
+                                const retorno = {
+                                    "type": "conn",
+                                    "data": data
+                                }
+                                clients[key].send(JSON.stringify(retorno));
+                            } else if (input["type"] == 'add') {
+
+                                const retorno = {
+                                    "type": "add",
+                                    "data": input["data"],
+                                    "row": input["row"]
+                                }
+                                //await updateData(client, "sameday", data)
+
+
+
+
+                                clients[key].send(JSON.stringify(retorno))
+                                console.log("--------------------------------------")
+                                console.log(clients[key].state)
+                                console.log("--------------------------------------")
+                            } else if (input["type"] == 'delete') {
+
+                                const retorno = {
+                                    "type": "delete",
+                                    "data": data
+                                }
+
+                                console.log(clients[key].send(JSON.stringify(retorno)))
+                                //await updateData(client, "sameday", data)
+
+
+                            }
+                            //console.log("Al final")
+
+
+                        }else{
+                            delete clients[key]
+                        }
                     }
                     await updateData(client, "sameday", data)
 
